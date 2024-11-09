570c98af58c498c7cb7bc7a1667fe0c4bb276f0a
Daniel Alvarez <ingdanielf8@gmail.com>
2024-11-09 17:42:13 -0500

    Fix sluder F and init buttons

--- src/components/SlyderF.vue
+++ src/components/SlyderF.vue
@@ -1,32 +1,32 @@
 <template lang="pug">
-.slyder-f
-  
-  .slyder-f__btn.pe-4(@click="clickAnterior")
-    i.fas.fa-chevron-left
-  .slyder-f__main(:id="'slyder-f-' + mainId")
-    ScrollHorizontal(
-      v-if="elements.length && rendered" 
-      :selectedId="'sl-' + selected"
-      row
-    )
-      .slyder-f__slyde(
-        v-for="(item, index) in elements"
-        :key="'sl-key-'+item.id"
-        :id="'sl-'+item.id"
-        :class="columnas"
-        v-child="item.elm"
+  .slyder-f
+    .slyder-f__btn.pe-4(@click="clickAnterior" :class="{'disabled': isFirstSlide}")
+      i.fas.fa-chevron-left
+    .slyder-f__main(:id="'slyder-f-' + mainId")
+      ScrollHorizontal(
+        v-if="elements.length && rendered" 
+        :selectedId="'sl-' + selected"
+        row
       )
-  .slyder-f__btn.ps-4(@click="clickSiguiente")
-    i.fas.fa-chevron-right
-
-  .hidden-slot
-    slot
+        .slyder-f__slyde(
+          v-for="(item, index) in elements"
+          :key="'sl-key-'+item.id"
+          :id="'sl-'+item.id"
+          :class="columnas"
+          v-child="item.elm"
+        )
+    .slyder-f__btn.ps-4(@click="clickSiguiente" :class="{'disabled': isLastSlide }")
+      i.fas.fa-chevron-right
+  
+    .hidden-slot
+      slot
 </template>
 
 <script>
 import ScrollHorizontal from './plantilla/ScrollHorizontal'
 import componentSlotMixins from '../mixins/componentSlotMixins'
 import slyderMixins from '../mixins/slyderMixins'
+
 export default {
   name: 'SlyderF',
   components: { ScrollHorizontal },
@@ -37,34 +37,52 @@ export default {
       default: '',
     },
   },
-  methods: {
-    clickAnterior() {
+  computed: {
+    isLastSlide() {
+      if (!this.elements.length) return true
       const ids = this.elements.map(element => element.id)
       const idxOfSelected = ids.indexOf(this.selected)
-      if (idxOfSelected > 0) {
-        this.selected = ids[idxOfSelected - 1]
-      }
+      return idxOfSelected === ids.length - 2
+    },
+
+    isFirstSlide() {
+      if (!this.elements.length) return true
+      const ids = this.elements.map(element => element.id)
+      const idxOfSelected = ids.indexOf(this.selected)
+      return idxOfSelected === 0
+    },
+  },
+  methods: {
+    clickAnterior() {
+      if (this.isFirstSlide) return
+      const ids = this.elements.map(element => element.id)
+      const idxOfSelected = ids.indexOf(this.selected)
+      this.selected = ids[idxOfSelected - 1]
     },
     clickSiguiente() {
+      if (this.isLastSlide) return
       const ids = this.elements.map(element => element.id)
-      const container = document.querySelector('#slyder-f-' + this.mainId)
-      const containerComputedStyles = window.getComputedStyle(container)
-      const containerWidth = parseFloat(containerComputedStyles.width)
-      const elm = document.querySelector(`#sl-${this.elements[0].id}`)
-      const elmComputedStyles = window.getComputedStyle(elm)
-      const elmWidth = parseFloat(elmComputedStyles.width)
-      const elmtsPerContainer = Math.round(containerWidth / elmWidth)
-      const newIds =
-        elmtsPerContainer > 1
-          ? ids.slice(0, ids.length - (elmtsPerContainer - 1))
-          : ids
-      const idxOfSelected = newIds.indexOf(this.selected)
-      if (idxOfSelected < newIds.length - 1) {
-        this.selected = newIds[idxOfSelected + 1]
-      }
+      const idxOfSelected = ids.indexOf(this.selected)
+      this.selected = ids[idxOfSelected + 1]
     },
   },
 }
 </script>
 
-<style lang="sass"></style>
+<style lang="sass">
+.slyder-f
+  &__btn
+    i
+      transition: all 0.3s ease-in-out
+    i.fa-chevron-left:hover
+      transform: translateX(-5px)
+    i.fa-chevron-right:hover
+      transform: translateX(5px)
+    &.disabled
+      i
+        opacity: 0.5
+        cursor: auto
+      i.fa-chevron-left:hover,
+      i.fa-chevron-right:hover
+        transform: none !important
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -137,4 +137,23 @@ export default {
 
   .footer p
     font-size: 14px
+
+.iniciar-btn-container
+  .iniciar-btn-text
+    transition: all 0.4s ease-in-out
+    background-color: #9EF0FD !important
+    color: #0B0A0A !important
+  .icon
+    transition: all 0.4s ease-in-out
+    background-color: #083E61 !important
+  &:hover
+    .iniciar-btn-text
+      background-color: #083E61 !important
+      color: white !important
+    .icon
+      background-color: #9EF0FD !important
+      color: #0B0A0A !important
+
+.home-main-title
+  color: #E5782C !important
 </style>
