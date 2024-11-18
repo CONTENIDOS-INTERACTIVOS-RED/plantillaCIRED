0cb05bdc3c4f5e72c7546b54d0ec80e8a9ddc68d
Daniel Francisco Alvarez Caballero <95438055+DanielFrnAlvarez@users.noreply.github.com>
2024-11-18 18:55:32 -0500

    Update BarraAvance.vue

--- src/components/plantilla/BarraAvance.vue
+++ src/components/plantilla/BarraAvance.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 .barra-avance(:class="[showBarra ? 'barra-avance--open' : 'barra-avance--close']")
 
-  span.me-auto.ps-2.ms-1 {{ `${menuData[0].unidad}` }}
+  span.me-auto.ps-2.ms-1 Unidad 
 
   router-link.barra-avance__boton--regresar(
     :class="[!controlLinks.back.name && 'barra-avance__boton--disable']"
